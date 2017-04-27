b82dcf83c33f7e7e5ea7deb81bb64a712567cef9
wangjiaojiao <jiaojiao.wang@w-oasis.com>
2017-04-27 17:40:02 +0800

    初始化项目

---
+++ 03-YFPageViewDemo/YFPageViewDemo/YFPageView/YFContentView.swift
@@ -0,0 +1,21 @@
+//
+//  YFContentView.swift
+//  YFPageViewDemo
+//
+//  Created by Allison on 2017/4/27.
+//  Copyright © 2017年 Allison. All rights reserved.
+//
+
+import UIKit
+
+class YFContentView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ 03-YFPageViewDemo/YFPageViewDemo/YFPageView/YFPageView.swift
@@ -0,0 +1,21 @@
+//
+//  YFPageView.swift
+//  YFPageViewDemo
+//
+//  Created by Allison on 2017/4/27.
+//  Copyright © 2017年 Allison. All rights reserved.
+//
+
+import UIKit
+
+class YFPageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

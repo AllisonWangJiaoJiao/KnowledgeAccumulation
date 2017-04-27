edda5cb64afdca6ab55b700ad03c196426a1738f
wangjiaojiao <jiaojiao.wang@w-oasis.com>
2017-04-27 15:10:16 +0800

    多图片粒子动画

--- 01-粒子动画效果/01-粒子动画效果/Emitterable.swift
+++ 01-粒子动画效果/01-粒子动画效果/Emitterable.swift
@@ -1,9 +1,9 @@
 //
 //  Emitterable.swift
-//  03-粒子动画效果
+//  01-粒子动画效果
 //
-//  Created by 小码哥 on 2016/12/10.
-//  Copyright © 2016年 xmg. All rights reserved.
+//  Created by Allison on 2017/4/27.
+//  Copyright © 2017年 Allison. All rights reserved.
 //
 
 import UIKit
@@ -25,37 +25,44 @@ extension Emitterable where Self : UIViewController {
         emitter.preservesDepth = true
         
         // 4.创建例子, 并且设置例子相关的属性
-        // 4.1.创建例子Cell
-        let cell = CAEmitterCell()
+        var cellsArr = [CAEmitterCell]()
         
-        // 4.2.设置粒子速度
-        cell.velocity = 150
-        cell.velocityRange = 100
-        
-        // 4.3.设置例子的大小
-        cell.scale = 0.7
-        cell.scaleRange = 0.3
-        
-        // 4.4.设置粒子方向
-        cell.emissionLongitude = CGFloat(-M_PI_2)
-        cell.emissionRange = CGFloat(M_PI_2 / 6)
-        
-        // 4.5.设置例子的存活时间
-        cell.lifetime = 3
-        cell.lifetimeRange = 1.5
-        
-        // 4.6.设置粒子旋转
-        cell.spin = CGFloat(M_PI_2)
-        cell.spinRange = CGFloat(M_PI_2 / 2)
-        
-        // 4.6.设置例子每秒弹出的个数
-        cell.birthRate = 10
-        
-        // 4.7.设置粒子展示的图片
-        cell.contents = UIImage(named: "good6_30x30")?.cgImage
+        for i in 0..<10 {
+            // 4.1.创建例子Cell
+            let cell = CAEmitterCell()
+            
+            // 4.2.设置粒子速度
+            cell.velocity = 150
+            cell.velocityRange = 100
+            
+            // 4.3.设置例子的大小
+            cell.scale = 0.7
+            cell.scaleRange = 0.3
+            
+            // 4.4.设置粒子方向
+            cell.emissionLongitude = CGFloat(-M_PI_2)
+            cell.emissionRange = CGFloat(M_PI_2 / 6)
+            
+            // 4.5.设置例子的存活时间
+            cell.lifetime = 3
+            cell.lifetimeRange = 1.5
+            
+            // 4.6.设置粒子旋转
+            cell.spin = CGFloat(M_PI_2)
+            cell.spinRange = CGFloat(M_PI_2 / 2)
+            
+            // 4.6.设置例子每秒弹出的个数
+            cell.birthRate = 2
+            
+            // 4.7.设置粒子展示的图片
+            cell.contents = UIImage(named: "good\(i)")?.cgImage
+            
+            //4.8添加到数组中
+            cellsArr.append(cell)
+        }
         
         // 5.将粒子设置到发射器中
-        emitter.emitterCells = [cell]
+        emitter.emitterCells = cellsArr
         
         // 6.将发射器的layer添加到父layer中
         view.layer.addSublayer(emitter)

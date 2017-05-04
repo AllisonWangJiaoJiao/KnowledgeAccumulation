0eb425e7804f792fcdad0d5866ef1c295d539aa4
wangjiaojiao <jiaojiao.wang@w-oasis.com>
2017-05-04 23:00:12 +0800

    友盟分享(手动继承)

--- 06-UmengShareDemo/UmengShareDemo/ViewController.swift
+++ 06-UmengShareDemo/UmengShareDemo/ViewController.swift
@@ -13,19 +13,32 @@ class ViewController: UIViewController {
    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+       shareAction_pic()
+        
+    }
+    func shareAction_pic (){
         UMSocialUIManager.showShareMenuViewInWindow { (platformType, userInfo) in
             print(platformType,userInfo)
+            
+            let messageObject : UMSocialMessageObject = UMSocialMessageObject.init()
+            messageObject.text = "新浪微博测试网站"
+            
+            //分享网页
+            let shareObject : UMShareWebpageObject = UMShareWebpageObject.init()
+            shareObject.title = "网站"
+            shareObject.descr = "测试测试网站"
+            shareObject.thumbImage = UIImage.init(named: "icon_circle")
+            shareObject.webpageUrl = "http://www.baidu.com"
+            messageObject.shareObject = shareObject
+            
+            UMSocialManager.default().share(to: platformType, messageObject: messageObject, currentViewController: self, completion: { (shareResponse, error) in
+                print(shareObject,error)
+            })
+            
         }
-        UMSocialUIManager.setShareMenuViewDelegate(self)
-        
         
     }
     
 }
 
-extension ViewController :UMSocialShareMenuViewDelegate{
-    //不需要改变父窗口则不需要重写此协议
-    func umSocialParentView(_ defaultSuperView: UIView!) -> UIView! {
-        return defaultSuperView
-    }
-}
+

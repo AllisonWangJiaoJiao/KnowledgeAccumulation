a2df9a91ddf523a9f0f7b01baa76f1eb3eb5f9b8
wangjiaojiao <jiaojiao.wang@w-oasis.com>
2017-05-05 10:11:43 +0800

    cocoaPods继承友盟分享

--- 06-UmengShareDemo/UmengShareDemo/AppDelegate.swift
+++ 06-UmengShareDemo/UmengShareDemo/AppDelegate.swift
@@ -31,7 +31,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UMSocialManager.default().setPlaform(UMSocialPlatformType.sina, appKey: "2690150259", appSecret: "c643e38611549d188cd6b62f7f3cc686", redirectURL: "http://weibo.com")
         
         /* 设置微信的appKey和appSecret */
-         UMSocialManager.default().setPlaform(UMSocialPlatformType.whatsapp, appKey: "wxdc1e388c3822c80b", appSecret: "3baf1193c85774b3fd9d18447d76cab0", redirectURL: "http://mobile.umeng.com/social")
+         UMSocialManager.default().setPlaform(UMSocialPlatformType.wechatSession, appKey: "wxdc1e388c3822c80b", appSecret: "3baf1193c85774b3fd9d18447d76cab0", redirectURL: "http://mobile.umeng.com/social")
+
         
     }
     

74cd22d3cf4694bf991be976dc647fd9886f7e7d
Allison <jiaojiao.wang@w-oasis.com>
2017-05-21 15:47:06 +0800

    YFPageView扩展

---
+++ 03-YFPageViewExtensionDemo/YFPageViewExtensionDemo/YFPageView/YFPageCollectionView.swift
@@ -0,0 +1,15 @@
+//
+//  YFPageCollectionView.swift
+//  YFPageViewExtensionDemo
+//
+//  Created by Allison on 2017/5/21.
+//  Copyright © 2017年 Allison. All rights reserved.
+//
+
+import UIKit
+
+class YFPageCollectionView: UIView {
+
+
+
+}

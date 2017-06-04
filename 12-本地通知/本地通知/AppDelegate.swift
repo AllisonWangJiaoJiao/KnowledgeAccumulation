c938628002eb3ebaa2f69bb3aa49d0e7a3de2551
Allison <jiaojiao.wang@w-oasis.com>
2017-06-04 21:10:45 +0800

    注册通知授权

--- 12-本地通知/本地通知/AppDelegate.swift
+++ 12-本地通知/本地通知/AppDelegate.swift
@@ -15,32 +15,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        
+        registerAuthor()
+        
         return true
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+    
+    //注册通知授权
+    func registerAuthor()  {
+        
+        if #available(iOS 8.0, *) {
+            let type = UIUserNotificationType.alert.rawValue | UIUserNotificationType.badge.rawValue | UIUserNotificationType.badge.rawValue
+            let sets = UIUserNotificationSettings(types: UIUserNotificationType(rawValue: type), categories: nil)
+            UIApplication.shared.registerUserNotificationSettings(sets)
+        }
     }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
 }
 
--- 12-本地通知/本地通知/ViewController.swift
+++ 12-本地通知/本地通知/ViewController.swift
@@ -15,11 +15,34 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //发送通知
+    //iOS8.0以后,需要主动请求授权,才可以发送通知
+    @IBAction func sendNotificationClick(_ sender: UIButton) {
+        
+        //1.创建一个本地通知
+        let localNot = UILocalNotification()
+        //1.1设置通知的必选项
+        localNot.alertBody = "顺子要不要"
+        //1.2通知的发送时间
+        localNot.fireDate = NSDate(timeIntervalSinceNow: 2) as Date
+
+        //2.发送本地通知
+        //应用程序界别的操作
+        UIApplication.shared.scheduleLocalNotification(localNot)
+        
+        
+    }
+   
+    //取消通知
+    @IBAction func cancleNotificationClick(_ sender: UIButton) {
+    }
+    
+    //查看通知
+    @IBAction func viewNotificationClick(_ sender: UIButton) {
     }
 
+    
+    
 
 }
 

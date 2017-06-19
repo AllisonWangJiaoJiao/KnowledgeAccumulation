b9b0bc52f6784d9f36aa4525349bf4513d3983bd
Allison <jiaojiao.wang@w-oasis.com>
2017-06-19 23:03:56 +0800

    识别图片二维码

--- 13.QRCodeDemo/QRCodeDemo/Classes/RecognitionQRCode/Controller/RecognitionQRCodeVC.swift
+++ 13.QRCodeDemo/QRCodeDemo/Classes/RecognitionQRCode/Controller/RecognitionQRCodeVC.swift
@@ -9,6 +9,33 @@
 import UIKit
 
 class RecognitionQRCodeVC: UIViewController {
+    
+    @IBOutlet weak var sourceImageView: UIImageView!
+    
+    
+    //开始识别
+    @IBAction func detectorQRCode(_ sender: UIButton) {
+        
+        // 1.获取需要识别的图片
+        let image = sourceImageView.image
+        let imageCI = CIImage(image: image!)
+        
+        
+        //开始识别
+        //1.创建一个二维码探测器
+        let dector = CIDetector(ofType: CIDetectorTypeQRCode, context: nil, options: [CIDetectorAccuracy:CIDetectorAccuracyHigh])
+        
+        
+        //2.直接探测二维码特征
+        let features = dector?.features(in: imageCI!)
+        for feature in features! {            
+            let qrFeature = feature as!CIQRCodeFeature
+            print(qrFeature.messageString)
+            print(qrFeature.bounds)
+        }
+        
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,20 +43,5 @@ class RecognitionQRCodeVC: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

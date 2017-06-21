faedf8ea062f40daf7674a90b871bcbf13e7b6f0
Allison <jiaojiao.wang@w-oasis.com>
2017-06-21 08:59:10 +0800

    扫码功能

--- 13.QRCodeDemo/QRCodeDemo/Classes/ScanQRCode/Controller/ScanQRCodeVC.swift
+++ 13.QRCodeDemo/QRCodeDemo/Classes/ScanQRCode/Controller/ScanQRCodeVC.swift
@@ -19,6 +19,7 @@ class ScanQRCodeVC: UIViewController {
     
     
     var session : AVCaptureSession?
+    weak var layer : AVCaptureVideoPreviewLayer?
     
 
     override func viewDidAppear(_ animated: Bool) {
@@ -69,6 +70,7 @@ class ScanQRCodeVC: UIViewController {
         layer?.frame = view.layer.bounds
         //view.layer.addSublayer(layer!)
         view.layer.insertSublayer(layer!, at: 0)
+        self.layer = layer
         
         //4.启动会话(让输入开始采集数据,输出对象,开始处理数据)
         session!.startRunning()
@@ -79,11 +81,53 @@ class ScanQRCodeVC: UIViewController {
 extension ScanQRCodeVC : AVCaptureMetadataOutputObjectsDelegate{
     //扫描到结果之后调用
     func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
-        print("test")
+        print(metadataObjects)
+        
+        for obj in metadataObjects {
+            if (obj as AnyObject).isKind(of: AVMetadataMachineReadableCodeObject.self) {
+                
+                //转换成为二维码,再预览图层上的真正坐标
+                let resultObj =  self.layer?.transformedMetadataObject(for: obj as! AVMetadataObject)
+    
+                let qrCodeObj = resultObj as! AVMetadataMachineReadableCodeObject
+                print(qrCodeObj.stringValue)
+                print("二维码的四个角:\(qrCodeObj.corners)")
+                //qrCodeObj.corners 代表二维码的四个角,但是,需要借助视频预览图层,转换为我们可以用的坐标.
+               
+            }
+        }
     }
     
     
-    
+    func drawFrame(qrCodeObj:AVMetadataMachineReadableCodeObject) -> () {
+        guard let corners = qrCodeObj.corners else {return}
+        
+        //1.借助一个图形层,来回执
+        let shapLayer = CAShapeLayer()
+        
+        //2.根据四个点,创建一个路径
+        let path = UIBezierPath()
+        for corner in corners {
+            
+            let pointDic = corners as! CFDictionary
+            var point = CGPoint.zero
+            
+            //CGPointMakeWithDictionaryRepresentation(pointDic, &point)
+            
+            var index = 0
+            //CGPointMakeWithDictionaryRepresentation(pointDic, &point)
+            
+        }
+        
+        
+        //3.给图形图层的路径赋值,代表图层展示怎样的形状
+        
+        //4.直接添加图形图层到需要展示的图层
+        
+        
+        
+        
+    }
     
 }
 

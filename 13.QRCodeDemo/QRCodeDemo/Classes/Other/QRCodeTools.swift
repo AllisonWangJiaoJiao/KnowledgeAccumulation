0e4a7081af9c8ba7d9d1ff3a8ba478b4239ba3fa
Allison <jiaojiao.wang@w-oasis.com>
2017-06-22 17:47:02 +0800

    二维码封装重构-2

--- 13.QRCodeDemo/QRCodeDemo/Classes/Other/QRCodeTools.swift
+++ 13.QRCodeDemo/QRCodeDemo/Classes/Other/QRCodeTools.swift
@@ -7,12 +7,121 @@
 //
 
 import UIKit
+import AVFoundation
+
+typealias ScanResultBlock = ([String])->()
 
 class QRCodeTools: NSObject {
     
+    ///设置单利对象
+    static let shareInstance = QRCodeTools()
+    
+    ///懒加载输入对象
+    lazy var input :AVCaptureDeviceInput? = {
+        //1.设置输入
+        //1.1获取摄像头设备
+        let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
+        //1.2把摄像头设备当做输入设备
+        var input : AVCaptureDeviceInput?
+        do {
+            input = try AVCaptureDeviceInput(device: device)
+            return input
+        } catch  {
+            print(error)
+            return nil
+        }
+    }()
+    
+    ///懒加载输出对象
+    lazy var output :AVCaptureMetadataOutput = {
+        //2.设置输出
+        let output = AVCaptureMetadataOutput()
+        //2.1设置结果处理的代理
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        return output
+    }()
+    
+    //懒加载会话对象
+    lazy var session : AVCaptureSession = {
+        //3.创建会话,连接输入和输出
+        let session = AVCaptureSession()
+        return session
+    }()
+    
+    ///懒加载layer
+    lazy var layer :AVCaptureVideoPreviewLayer? = {
+        let layer =  AVCaptureVideoPreviewLayer(session: self.session)
+        return layer
+    }()
+    
+    ///闭包
+   fileprivate var scanResultBlock : ScanResultBlock?
+    //记录
+    fileprivate var isDrawFrame: Bool = false
+
+    //扫描二维码
+    func scanQRCode(inView:UIView,isDrawFrame:Bool, resultBlock:@escaping (_ resultStrs:[String])->()){
+        //记录闭包
+        scanResultBlock = resultBlock
+        //记录绘制边框
+        self.isDrawFrame = isDrawFrame
+        
+        if session.canAddInput(input) && session.canAddOutput(output) {
+            session.addInput(input)
+            session.addOutput(output)
+        }else{
+            return
+        }
+
+        //3.1设置二维码可以识别的码制
+        //设置识别的类型,必须要在输出添加到会话之后,才可以设置,不然crash
+        //output.availableMetadataObjectTypes
+        //[AVMetadataObjectTypeQRCode]
+        output.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+        
+        
+        
+        //3.2添加视频预览图层(让用户可以看到界面)(不是不许添加的)
+        if inView.layer.sublayers == nil {
+            layer?.frame = inView.layer.bounds
+            //view.layer.addSublayer(layer!)
+            inView.layer.insertSublayer(layer!, at: 0)
+        }else{
+            let subLayers = inView.layer.sublayers!
+            
+            if !subLayers.contains(layer!) {
+                layer?.frame = inView.layer.bounds
+                inView.layer.insertSublayer(layer!, at: 0)
+            }
+        }
+ 
+        //4.启动会话(让输入开始采集数据,输出对象,开始处理数据)
+        session.startRunning()
+
+    }
+    
+    //设置扫码的兴趣区域
+    func setRectInterest(orignRect:CGRect) -> () {
+        //CGRectMake(0, 0, 1, 1)
+        //0,0 右上角
+        let bounds = UIScreen.main.bounds
+        let x :CGFloat = orignRect.origin.x / bounds.size.width
+        let y :CGFloat = orignRect.origin.y / bounds.size.height
+        let width : CGFloat = orignRect.size.width / bounds.size.width
+        let height : CGFloat = orignRect.size.height / bounds.size.height
+        output.rectOfInterest = CGRect(x: y, y: x, width: height, height: width)
+        //output.rectOfInterest = CGRect(x: x, y: y, width: width, height: height)
+   
+    }
     
     
-    //识别图片二维码
+    
+    /// 识别图片二维码
+    ///
+    /// - Parameters:
+    ///   - image: 原始图片
+    ///   - isDrawQRCodeFrame: 是否需要绘制扫描边框
+    /// - Returns: 元组(结果字符串组成的数组,绘制好二维码边框的图片(如果不要求绘制,则返回的是原始图片))
     class func recognitionQRCode(image:UIImage,isDrawQRCodeFrame:Bool)->(resultStr:[String]?,resultImage:UIImage) {
         
         let imageCI = CIImage(image: image)
@@ -40,7 +149,13 @@ class QRCodeTools: NSObject {
         
     }
     
-    //生成二维码
+    
+    /// 生成二维码
+    ///
+    /// - Parameters:
+    ///   - inputStr: 需要转换的字符串
+    ///   - centerImage: 字符串中间的自定义图片
+    /// - Returns: 生成后的结果图片
     class func generateQRCode(inputStr:String,centerImage:UIImage?) -> UIImage {
         
         // 1.创建二维码滤镜
@@ -141,3 +256,92 @@ extension QRCodeTools {
         return resultImage!
     }
 }
+
+
+//MARK:AVCaptureMetadataOutput
+extension QRCodeTools : AVCaptureMetadataOutputObjectsDelegate{
+        //扫描到结果之后调用
+        func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
+            print(metadataObjects)
+            
+            if isDrawFrame {
+                removeFrameLayer()
+            }
+            
+            var resultStrsArr = [String]()
+            for obj in metadataObjects {
+                if (obj as AnyObject).isKind(of: AVMetadataMachineReadableCodeObject.self) {
+                    
+                    //转换成为二维码,再预览图层上的真正坐标
+                    let resultObj =  layer?.transformedMetadataObject(for: obj as! AVMetadataObject)
+                    let qrCodeObj = resultObj as! AVMetadataMachineReadableCodeObject
+                    //将数据添加到数组
+                    resultStrsArr.append(qrCodeObj.stringValue)
+                    //print(qrCodeObj.stringValue)
+                    //print("二维码的四个角:\(qrCodeObj.corners)")
+                    //qrCodeObj.corners 代表二维码的四个角,但是,需要借助视频预览图层,转换为我们可以用的坐标.
+                    if isDrawFrame {
+                         drawFrame(qrCodeObj: qrCodeObj)
+                    }
+                }
+            }
+            
+            if let scanResultBlock  = scanResultBlock {
+                scanResultBlock(resultStrsArr)
+            }
+            
+//            if scanResultBlock != nil {
+//                scanResultBlock(resultStrsArr)
+//            }
+        }
+        
+        
+        func drawFrame(qrCodeObj:AVMetadataMachineReadableCodeObject) -> () {
+            guard let cornersArr = qrCodeObj.corners else {return}
+            
+            //1.借助一个图形层,来回执
+            let shapLayer = CAShapeLayer()
+            shapLayer.fillColor = UIColor.clear.cgColor
+            shapLayer.strokeColor = UIColor.red.cgColor
+            shapLayer.lineWidth = 3
+            
+            //2.根据四个点,创建一个路径
+            let path = UIBezierPath()
+            var index = 0
+            
+            for corner in cornersArr {
+                let pointDic = corner as! CFDictionary
+                var point = CGPoint.zero
+                point = CGPoint.init(dictionaryRepresentation: pointDic)!
+                
+                //第一个点
+                if index == 0 {
+                    path.move(to: point)
+                }else{
+                    path.addLine(to: point)
+                }
+                index += 1
+            }
+            path.close()
+            
+            //3.给图形图层的路径赋值,代表图层展示怎样的形状
+            shapLayer.path = path.cgPath
+            
+            //4.直接添加图形图层到需要展示的图层
+            layer?.addSublayer(shapLayer)
+        }
+        
+        func removeFrameLayer() -> () {
+            guard let subLayers = layer?.sublayers else {return}
+            
+            for subLayer in subLayers {
+                if subLayer.isKind(of: CAShapeLayer.self) {
+                    subLayer.removeFromSuperlayer()
+                }
+            }
+        }
+        
+}
+
+
+
--- 13.QRCodeDemo/QRCodeDemo/Classes/ScanQRCode/Controller/ScanQRCodeVC.swift
+++ 13.QRCodeDemo/QRCodeDemo/Classes/ScanQRCode/Controller/ScanQRCodeVC.swift
@@ -18,7 +18,7 @@ class ScanQRCodeVC: UIViewController {
     @IBOutlet weak var toBottom: NSLayoutConstraint!
     
     
-    var session : AVCaptureSession?
+  
     weak var layer : AVCaptureVideoPreviewLayer?
     
 
@@ -33,128 +33,14 @@ class ScanQRCodeVC: UIViewController {
     
     func startScan() -> () {
         
-        //1.获取摄像头设备
-        //1.1获取摄像头设备
-        let device = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeVideo)
-        //1.2把摄像头设备当做输入设备
-        var input : AVCaptureDeviceInput?
-        do {
-            input = try AVCaptureDeviceInput(device: device)
-        } catch  {
-            print(error)
-            return
-        }
-        
-        //2.设置输出
-        let output = AVCaptureMetadataOutput()
-        //2.1设置结果处理的代理
-        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-        
-        //3.创建会话,连接输入和输出
-        session = AVCaptureSession()
-        if session!.canAddInput(input) && session!.canAddOutput(output) {
-            session!.addInput(input)
-            session!.addOutput(output)
-        }else{
-            return
-        }
-        //3.1设置二维码可以识别的码制
-        //设置识别的类型,必须要在输出添加到会话之后,才可以设置,不然crash
-        //output.availableMetadataObjectTypes
-        //[AVMetadataObjectTypeQRCode]
-        output.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
-        
-        //设置扫码的兴趣区域
-        //CGRectMake(0, 0, 1, 1)
-        //0,0 右上角 
-        let bounds = UIScreen.main.bounds
-        let x :CGFloat = scanBackView.frame.origin.x / bounds.size.width
-        let y :CGFloat = scanBackView.frame.origin.y / bounds.size.height
-        let width : CGFloat = scanBackView.frame.size.width / bounds.size.width
-        let height : CGFloat = scanBackView.frame.size.height / bounds.size.height
-        output.rectOfInterest = CGRect(x: y, y: x, width: height, height: width)
-        //output.rectOfInterest = CGRect(x: x, y: y, width: width, height: height)
-
-        //3.2添加视频预览图层(让用户可以看到界面)(不是不许添加的)
-        let layer  = AVCaptureVideoPreviewLayer(session: session)
-        layer?.frame = view.layer.bounds
-        //view.layer.addSublayer(layer!)
-        view.layer.insertSublayer(layer!, at: 0)
-        self.layer = layer
-        
-        //4.启动会话(让输入开始采集数据,输出对象,开始处理数据)
-        session!.startRunning()
-    }
-}
-
-//MARK:AVCaptureMetadataOutput
-extension ScanQRCodeVC : AVCaptureMetadataOutputObjectsDelegate{
-    //扫描到结果之后调用
-    func captureOutput(_ captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [Any]!, from connection: AVCaptureConnection!) {
-        print(metadataObjects)
-        
-        removeFrameLayer()
-        for obj in metadataObjects {
-            if (obj as AnyObject).isKind(of: AVMetadataMachineReadableCodeObject.self) {
-                
-                //转换成为二维码,再预览图层上的真正坐标
-                let resultObj =  self.layer?.transformedMetadataObject(for: obj as! AVMetadataObject)
-    
-                let qrCodeObj = resultObj as! AVMetadataMachineReadableCodeObject
-                print(qrCodeObj.stringValue)
-                print("二维码的四个角:\(qrCodeObj.corners)")
-                //qrCodeObj.corners 代表二维码的四个角,但是,需要借助视频预览图层,转换为我们可以用的坐标.
-               drawFrame(qrCodeObj: qrCodeObj)
-            }
+//        QRCodeTools.shareInstance.scanQRCode(inView: view) { (result:[String]) in
+//            print(result)
+//        }
+        QRCodeTools.shareInstance.setRectInterest(orignRect: scanBackView.frame)
+        QRCodeTools.shareInstance.scanQRCode(inView: view, isDrawFrame: true) { (result:[String]) in
+            print(result)
         }
     }
-    
-    
-    func drawFrame(qrCodeObj:AVMetadataMachineReadableCodeObject) -> () {
-        guard let cornersArr = qrCodeObj.corners else {return}
-        
-        //1.借助一个图形层,来回执
-        let shapLayer = CAShapeLayer()
-        shapLayer.fillColor = UIColor.clear.cgColor
-        shapLayer.strokeColor = UIColor.red.cgColor
-        shapLayer.lineWidth = 3
-        
-        //2.根据四个点,创建一个路径
-        let path = UIBezierPath()
-        var index = 0
-        
-        for corner in cornersArr {
-            let pointDic = corner as! CFDictionary
-            var point = CGPoint.zero
-            point = CGPoint.init(dictionaryRepresentation: pointDic)!
-            
-            //第一个点
-            if index == 0 {
-                path.move(to: point)
-            }else{
-                path.addLine(to: point)
-            }
-            index += 1
-        }
-        path.close()
-        
-        //3.给图形图层的路径赋值,代表图层展示怎样的形状
-        shapLayer.path = path.cgPath
-        
-        //4.直接添加图形图层到需要展示的图层
-        layer?.addSublayer(shapLayer)
-    }
-    
-    func removeFrameLayer() -> () {
-        guard let subLayers = layer?.sublayers else {return}
-        
-        for subLayer in subLayers {
-            if subLayer.isKind(of: CAShapeLayer.self) {
-                subLayer.removeFromSuperlayer()
-            }
-        }
-    }
-    
 }
 
 extension ScanQRCodeVC {

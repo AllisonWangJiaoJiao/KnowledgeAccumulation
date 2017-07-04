2280a26db3342e92aa14549e104caf5a67e97129
Allison <jiaojiao.wang@w-oasis.com>
2017-07-04 22:34:19 +0800

    服务器开始监听

--- 14.Server/Server/Server/ServerManager.swift
+++ 14.Server/Server/Server/ServerManager.swift
@@ -10,11 +10,29 @@ import Cocoa
 
 class ServerManager: NSObject {
 
+    fileprivate lazy var serverSocket : TCPServer = TCPServer.init(addr: "0.0.0.0", port: 7878)
+    
+    
 }
 
 extension ServerManager {
     
     func statrRuning() {
+        
+        //1.开启监听
+        serverSocket.listen()
+        
+        //2.开始接受客户端
+        DispatchQueue.global().async {
+            if let client = self.serverSocket.accept(){
+             print("接受到一个客服端的连接")
+            }
+        }
+        
+        //3.
+        
+        
+        
         
     }
     

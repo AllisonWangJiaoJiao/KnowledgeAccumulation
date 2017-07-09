90a2b31077321da533e557a8d510ba9ce992562e
Allison <jiaojiao.wang@w-oasis.com>
2017-07-09 17:23:58 +0800

    Mac

--- 15.Client/Client/ViewController.swift
+++ 15.Client/Client/ViewController.swift
@@ -32,14 +32,21 @@ class ViewController: UIViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
         //1.获取消息的长度
-        let message = "你好啊, 王小二....."
-        let data = message.data(using: String.Encoding.utf8)!
-        var length = data.count
-        print(length)
+//        let message = "你好啊, 王小二....."
+//        let data = message.data(using: String.Encoding.utf8)!
+//        var length = data.count
+//        print(length)
+        
+        let userInfo = UserInfo.Builder()
+        userInfo.name = "why\(arc4random())"
+        userInfo.level = 20
+        let msgData = (try! userInfo.build()).data()
         
         
         //2.将消息长度写入到data
+        var length = msgData.count
         let headerData = Data(bytes: &length, count: 4)
+       
         
         //3.消息类型
         var type = 2
@@ -49,7 +56,7 @@ class ViewController: UIViewController {
         
         //3.发送消息
         //消息格式: 1.消息长度 + 2.消息类型 + 3.真实消息
-        let totalData = headerData + typeData + data
+        let totalData = headerData + typeData + msgData
         socket.sendMsg(data: totalData)
     }
 
